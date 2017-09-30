DNAProject/DNA

only current bookkeepers can add or delete bookkeeper
Add a issuer for add/sub bookkeeper command.
Only the current bookkeepers can add other peers as
bookkeeper or delete other bookkeepers.

Signed-off-by: Jin Qing <1091147665@qq.com>

cli/bookkeeper/bookkeeper.go
@@ -1,7 +1,10 @@
 package bookkeeper
 
 import (
+	"DNA/account"
 	. "DNA/cli/common"
+	"DNA/core/contract"
+	"DNA/core/signature"
 	"DNA/core/transaction"
 	"DNA/crypto"
 	"DNA/net/httpjsonrpc"
@@ -15,17 +18,49 @@ import (
 	"github.com/urfave/cli"
 )
 
-func makeBookkeeperTransaction(pubkey *crypto.PubKey, op bool, cert []byte) (string, error) {
-	tx, _ := transaction.NewBookKeeperTransaction(pubkey, op, cert)
+func makeBookkeeperTransaction(pubkey *crypto.PubKey, op bool, cert []byte, issuer *account.Account) (string, error) {
+	tx, _ := transaction.NewBookKeeperTransaction(pubkey, op, cert, issuer.PubKey())
 	attr := transaction.NewTxAttribute(transaction.Nonce, []byte(strconv.FormatInt(rand.Int63(), 10)))
 	tx.Attributes = make([]*transaction.TxAttribute, 0)
 	tx.Attributes = append(tx.Attributes, &attr)
+	if err := signTransaction(issuer, tx); err != nil {
+		fmt.Println("Sign regist transaction failed.")
+		return "", err
+	}
 	var buffer bytes.Buffer
 	if err := tx.Serialize(&buffer); err != nil {
-		fmt.Println("serialize bookkeeper transaction failed")
+		fmt.Println("Serialize bookkeeper transaction failed.")
 		return "", err
 	}
 	return hex.EncodeToString(buffer.Bytes()), nil
+}
+
+func newContractContextWithoutProgramHashes(data signature.SignableData) *contract.ContractContext {
+	return &contract.ContractContext{
+		Data:       data,
+		Codes:      make([][]byte, 1),
+		Parameters: make([][][]byte, 1),
+	}
+}
+
+func signTransaction(signer *account.Account, tx *transaction.Transaction) error {
+	signature, err := signature.SignBySigner(tx, signer)
+	if err != nil {
+		fmt.Println("SignBySigner failed.")
+		return err
+	}
+	transactionContract, err := contract.CreateSignatureContract(signer.PubKey())
+	if err != nil {
+		fmt.Println("CreateSignatureContract failed.")
+		return err
+	}
+	transactionContractContext := contract.NewContractContext(tx)
+	if err := transactionContractContext.AddContract(transactionContract, signer.PubKey(), signature); err != nil {
+		fmt.Println("AddContract failed")
+		return err
+	}
+	tx.SetPrograms(transactionContractContext.GetPrograms())
+	return nil
 }
 
 func assetAction(c *cli.Context) error {
@@ -65,16 +100,27 @@ func assetAction(c *cli.Context) error {
 		return nil
 	}
 	cert := c.String("cert")
-	txHex, err := makeBookkeeperTransaction(pubkey, add, []byte(cert))
+
+	wallet := account.Open(account.WalletFileName, WalletPassword(c.String("password")))
+	if wallet == nil {
+		fmt.Println("Failed to open wallet.")
+		os.Exit(1)
+	}
+
+	acc, _ := wallet.GetDefaultAccount()
+	txHex, err := makeBookkeeperTransaction(pubkey, add, []byte(cert), acc)
 	if err != nil {
 		return err
 	}
+
 	resp, err := httpjsonrpc.Call(Address(), "sendrawtransaction", 0, []interface{}{txHex})
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		return err
 	}
+
 	FormatOutput(resp)
+
 	return nil
 }
 

core/validation/txValidator.go
@@ -7,6 +7,7 @@ import (
 	"DNA/core/ledger"
 	tx "DNA/core/transaction"
 	"DNA/core/transaction/payload"
+	"DNA/crypto"
 	. "DNA/errors"
 	"errors"
 	"fmt"
@@ -27,7 +28,7 @@ func VerifyTransaction(Tx *tx.Transaction) ErrCode {
 	}
 
 	if err := CheckTransactionBalance(Tx); err != nil {
-		log.Warn("VerifyTransaction:", err)
+		log.Warn("[VerifyTransaction],", err)
 		return ErrTransactionBalance
 	}
 
@@ -238,12 +239,29 @@ func checkAmountPrecise(amount common.Fixed64, precision byte) bool {
 	return amount.GetData()%int64(math.Pow(10, 8-float64(precision))) != 0
 }
 
+func checkIssuerInBookkeeperList(issuer *crypto.PubKey, bookKeepers []*crypto.PubKey) bool {
+	for _, bk := range bookKeepers {
+		r := crypto.Equal(issuer, bk)
+		if r == true {
+			log.Debug("issuer is in bookkeeperlist")
+			return true
+		}
+	}
+	log.Debug("issuer is NOT in bookkeeperlist")
+	return false
+}
+
 func CheckTransactionPayload(Tx *tx.Transaction) error {
 
 	switch pld := Tx.Payload.(type) {
 	case *payload.BookKeeper:
 		//Todo: validate bookKeeper Cert
 		_ = pld.Cert
+		bookKeepers, _, _ := ledger.DefaultLedger.Store.GetBookKeeperList()
+		r := checkIssuerInBookkeeperList(pld.Issuer, bookKeepers)
+		if r == false {
+			return errors.New("The issuer isn't bookekeeper, can't add other in bookkeepers list.")
+		}
 		return nil
 	case *payload.RegisterAsset:
 		if pld.Asset.Precision < asset.MinPrecision || pld.Asset.Precision > asset.MaxPrecision {

elastos/Elastos.ELA

cli/bookkeeper/bookkeeper.go
@@ -1,7 +1,10 @@
 package bookkeeper
 
 import (
+	"DNA_POW/account"
 	. "DNA_POW/cli/common"
+	"DNA_POW/core/contract"
+	"DNA_POW/core/signature"
 	"DNA_POW/core/transaction"
 	"DNA_POW/crypto"
 	"DNA_POW/net/httpjsonrpc"
@@ -15,17 +18,49 @@ import (
 	"github.com/urfave/cli"
 )
 
-func makeBookkeeperTransaction(pubkey *crypto.PubKey, op bool, cert []byte) (string, error) {
-	tx, _ := transaction.NewBookKeeperTransaction(pubkey, op, cert)
+func makeBookkeeperTransaction(pubkey *crypto.PubKey, op bool, cert []byte, issuer *account.Account) (string, error) {
+	tx, _ := transaction.NewBookKeeperTransaction(pubkey, op, cert, issuer.PubKey())
 	attr := transaction.NewTxAttribute(transaction.Nonce, []byte(strconv.FormatInt(rand.Int63(), 10)))
 	tx.Attributes = make([]*transaction.TxAttribute, 0)
 	tx.Attributes = append(tx.Attributes, &attr)
+	if err := signTransaction(issuer, tx); err != nil {
+		fmt.Println("Sign regist transaction failed.")
+		return "", err
+	}
 	var buffer bytes.Buffer
 	if err := tx.Serialize(&buffer); err != nil {
-		fmt.Println("serialize bookkeeper transaction failed")
+		fmt.Println("Serialize bookkeeper transaction failed.")
 		return "", err
 	}
 	return hex.EncodeToString(buffer.Bytes()), nil
+}
+
+func newContractContextWithoutProgramHashes(data signature.SignableData) *contract.ContractContext {
+	return &contract.ContractContext{
+		Data:       data,
+		Codes:      make([][]byte, 1),
+		Parameters: make([][][]byte, 1),
+	}
+}
+
+func signTransaction(signer *account.Account, tx *transaction.Transaction) error {
+	signature, err := signature.SignBySigner(tx, signer)
+	if err != nil {
+		fmt.Println("SignBySigner failed.")
+		return err
+	}
+	transactionContract, err := contract.CreateSignatureContract(signer.PubKey())
+	if err != nil {
+		fmt.Println("CreateSignatureContract failed.")
+		return err
+	}
+	transactionContractContext := contract.NewContractContext(tx)
+	if err := transactionContractContext.AddContract(transactionContract, signer.PubKey(), signature); err != nil {
+		fmt.Println("AddContract failed")
+		return err
+	}
+	tx.SetPrograms(transactionContractContext.GetPrograms())
+	return nil
 }
 
 func assetAction(c *cli.Context) error {
@@ -65,16 +100,27 @@ func assetAction(c *cli.Context) error {
 		return nil
 	}
 	cert := c.String("cert")
-	txHex, err := makeBookkeeperTransaction(pubkey, add, []byte(cert))
+
+	wallet := account.Open(account.WalletFileName, WalletPassword(c.String("password")))
+	if wallet == nil {
+		fmt.Println("Failed to open wallet.")
+		os.Exit(1)
+	}
+
+	acc, _ := wallet.GetDefaultAccount()
+	txHex, err := makeBookkeeperTransaction(pubkey, add, []byte(cert), acc)
 	if err != nil {
 		return err
 	}
+
 	resp, err := httpjsonrpc.Call(Address(), "sendrawtransaction", 0, []interface{}{txHex})
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		return err
 	}
+
 	FormatOutput(resp)
+
 	return nil
 }
 

core/validation/txValidator.go
@@ -7,6 +7,7 @@ import (
 	"DNA_POW/core/ledger"
 	tx "DNA_POW/core/transaction"
 	"DNA_POW/core/transaction/payload"
+	"DNA_POW/crypto"
 	. "DNA_POW/errors"
 	"errors"
 	"fmt"
@@ -27,7 +28,7 @@ func VerifyTransaction(Tx *tx.Transaction) ErrCode {
 	}
 
 	if err := CheckTransactionBalance(Tx); err != nil {
-		log.Warn("VerifyTransaction:", err)
+		log.Warn("[VerifyTransaction],", err)
 		return ErrTransactionBalance
 	}
 
@@ -238,12 +239,29 @@ func checkAmountPrecise(amount common.Fixed64, precision byte) bool {
 	return amount.GetData()%int64(math.Pow(10, 8-float64(precision))) != 0
 }
 
+func checkIssuerInBookkeeperList(issuer *crypto.PubKey, bookKeepers []*crypto.PubKey) bool {
+	for _, bk := range bookKeepers {
+		r := crypto.Equal(issuer, bk)
+		if r == true {
+			log.Debug("issuer is in bookkeeperlist")
+			return true
+		}
+	}
+	log.Debug("issuer is NOT in bookkeeperlist")
+	return false
+}
+
 func CheckTransactionPayload(Tx *tx.Transaction) error {
 
 	switch pld := Tx.Payload.(type) {
 	case *payload.BookKeeper:
 		//Todo: validate bookKeeper Cert
 		_ = pld.Cert
+		bookKeepers, _, _ := ledger.DefaultLedger.Store.GetBookKeeperList()
+		r := checkIssuerInBookkeeperList(pld.Issuer, bookKeepers)
+		if r == false {
+			return errors.New("The issuer isn't bookekeeper, can't add other in bookkeepers list.")
+		}
 		return nil
 	case *payload.RegisterAsset:
 		if pld.Asset.Precision < asset.MinPrecision || pld.Asset.Precision > asset.MaxPrecision {
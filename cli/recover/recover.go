barbapapait/ft-blockchain

MV recover subcommand
Signed-off-by: Ziyan Zhou <zhouziyan@hotmail.com>

cli/recover/recover.go
@@ -1,0 +1,58 @@
+package recover
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"DNA/account"
+	. "DNA/cli/common"
+	"DNA/common/password"
+	"github.com/urfave/cli"
+)
+
+func recoverAction(c *cli.Context) error {
+	if c.NumFlags() == 0 {
+		cli.ShowSubcommandHelp(c)
+		return nil
+	}
+
+	privateKey := c.String("key")
+	if privateKey == "" {
+		fmt.Println("missing -k,--key option")
+		os.Exit(1)
+	}
+	newPassword, err := password.GetConfirmedPassword()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	newWalletName := fmt.Sprintf("wallet-%s-recovered.dat", time.Now().Format("2006-01-02-15-04-05"))
+	_, err = account.Recover(newWalletName, []byte(newPassword), privateKey)
+	if err != nil {
+		fmt.Println("failed to recover wallet from private key")
+		os.Exit(1)
+	}
+	fmt.Println("wallet is recovered successfully")
+	return nil
+}
+
+func NewCommand() *cli.Command {
+	return &cli.Command{
+		Name:        "recover",
+		Usage:       "recover wallet from private key",
+		Description: "With nodectl recover, you could recover your asset.",
+		ArgsUsage:   "[args]",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "key, k",
+				Usage: "private key",
+			},
+		},
+		Action: recoverAction,
+		OnUsageError: func(c *cli.Context, err error, isSubcommand bool) error {
+			PrintError(c, err, "wallet")
+			return cli.NewExitError("", 1)
+		},
+	}
+}

nodectl.go
@@ -13,6 +13,7 @@ import (
 	"DNA/cli/debug"
 	"DNA/cli/info"
 	"DNA/cli/privpayload"
+	"DNA/cli/recover"
 	"DNA/cli/test"
 	"DNA/cli/wallet"
 
@@ -46,6 +47,7 @@ func main() {
 		*privpayload.NewCommand(),
 		*data.NewCommand(),
 		*bookkeeper.NewCommand(),
+		*recover.NewCommand(),
 	}
 	sort.Sort(cli.CommandsByName(app.Commands))
 	sort.Sort(cli.FlagsByName(app.Flags))

elastos/Elastos.ELA

cli/recover/recover.go
@@ -1,0 +1,58 @@
+package recover
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"DNA_POW/account"
+	. "DNA_POW/cli/common"
+	"DNA_POW/common/password"
+	"github.com/urfave/cli"
+)
+
+func recoverAction(c *cli.Context) error {
+	if c.NumFlags() == 0 {
+		cli.ShowSubcommandHelp(c)
+		return nil
+	}
+
+	privateKey := c.String("key")
+	if privateKey == "" {
+		fmt.Println("missing -k,--key option")
+		os.Exit(1)
+	}
+	newPassword, err := password.GetConfirmedPassword()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	newWalletName := fmt.Sprintf("wallet-%s-recovered.dat", time.Now().Format("2006-01-02-15-04-05"))
+	_, err = account.Recover(newWalletName, []byte(newPassword), privateKey)
+	if err != nil {
+		fmt.Println("failed to recover wallet from private key")
+		os.Exit(1)
+	}
+	fmt.Println("wallet is recovered successfully")
+	return nil
+}
+
+func NewCommand() *cli.Command {
+	return &cli.Command{
+		Name:        "recover",
+		Usage:       "recover wallet from private key",
+		Description: "With nodectl recover, you could recover your asset.",
+		ArgsUsage:   "[args]",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "key, k",
+				Usage: "private key",
+			},
+		},
+		Action: recoverAction,
+		OnUsageError: func(c *cli.Context, err error, isSubcommand bool) error {
+			PrintError(c, err, "wallet")
+			return cli.NewExitError("", 1)
+		},
+	}
+}

nodectl.go
@@ -12,6 +12,7 @@ import (
 	"DNA_POW/cli/debug"
 	"DNA_POW/cli/info"
 	"DNA_POW/cli/privpayload"
+	"DNA_POW/cli/recover"
 	"DNA_POW/cli/test"
 	"DNA_POW/cli/wallet"
 
@@ -44,6 +45,7 @@ func main() {
 		*privpayload.NewCommand(),
 		*data.NewCommand(),
 		*bookkeeper.NewCommand(),
+		*recover.NewCommand(),
 	}
 	sort.Sort(cli.CommandsByName(app.Commands))
 	sort.Sort(cli.FlagsByName(app.Flags))
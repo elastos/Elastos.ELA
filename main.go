DNAProject/DNA

Add node information http server support
1.Add node information http server support, visit
http://nodeAddr:HttpInfoPort/info to get node information.

Signed-off-by: XiaoJie Guo <guoxiaojie@onchain.com>

main.go
@@ -13,6 +13,7 @@ import (
 	"DNA/net/httpjsonrpc"
 	"DNA/net/httprestful"
 	"DNA/net/httpwebsocket"
+	"DNA/net/httpnodeinfo"
 	"DNA/net/protocol"
 	"os"
 	"runtime"
@@ -102,6 +103,10 @@ func main() {
 	go httpjsonrpc.StartLocalServer()
 	go httprestful.StartServer(noder)
 	go httpwebsocket.StartServer(noder)
+	if config.Parameters.HttpInfoStart {
+		go httpnodeinfo.StartServer(noder)
+	}
+
 
 	for {
 		time.Sleep(dbft.GenBlockTime)

net/httpnodeinfo/server.go
@@ -1,0 +1,113 @@
+package httpnodeinfo
+
+import (
+	"DNA/common/config"
+	"DNA/core/ledger"
+	. "DNA/net/protocol"
+	"fmt"
+	"html/template"
+	"net/http"
+	"sort"
+	"strconv"
+)
+
+type Info struct {
+	NodeVersion   string
+	BlockHeight   uint32
+	NeighborCnt   int
+	Neighbors     []NgbNodeInfo
+	HttpRestPort  int
+	HttpWsPort    int
+	HttpJsonPort  int
+	HttpLocalPort int
+	NodePort      int
+	NodeId        string
+	NodeType      string
+}
+
+const (
+	verifyNode  = "Verify Node"
+	serviceNode = "Service Node"
+)
+
+var node Noder
+
+var templates = template.Must(template.New("info").Parse(page))
+
+func newNgbNodeInfo(ngbId string, ngbType string, ngbAddr string, httpInfoAddr string, httpInfoPort int, httpInfoStart bool) *NgbNodeInfo {
+	return &NgbNodeInfo{NgbId: ngbId, NgbType: ngbType, NgbAddr: ngbAddr, HttpInfoAddr: httpInfoAddr,
+		HttpInfoPort: httpInfoPort,	HttpInfoStart: httpInfoStart}
+}
+
+func initPageInfo(blockHeight uint32, curNodeType string, ngbrCnt int, ngbrsInfo []NgbNodeInfo) (*Info, error) {
+	id := fmt.Sprintf("0x%x", node.GetID())
+	return &Info{NodeVersion: config.Version, BlockHeight: blockHeight,
+		NeighborCnt: ngbrCnt, Neighbors: ngbrsInfo,
+		HttpRestPort:  config.Parameters.HttpRestPort,
+		HttpWsPort:    config.Parameters.HttpWsPort,
+		HttpJsonPort:  config.Parameters.HttpJsonPort,
+		HttpLocalPort: config.Parameters.HttpLocalPort,
+		NodePort:      config.Parameters.NodePort,
+		NodeId:        id, NodeType: curNodeType}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	var ngbrNodersInfo []NgbNodeInfo
+	var ngbId string
+	var ngbAddr string
+	var ngbType string
+	var ngbInfoPort int
+	var ngbInfoState bool
+	var ngbHttpInfoAddr string
+
+	curNodeType := serviceNode
+	bookKeepers, _, _ := ledger.DefaultLedger.Store.GetBookKeeperList()
+	bookKeeperLen := len(bookKeepers)
+	for i := 0; i < bookKeeperLen; i++ {
+		if node.GetPubKey().X.Cmp(bookKeepers[i].X) == 0 {
+			curNodeType = verifyNode
+			break
+		}
+	}
+
+	ngbrNoders := node.GetNeighborNoder()
+	ngbrsLen := len(ngbrNoders)
+	for i := 0; i < ngbrsLen; i++ {
+		ngbType = serviceNode
+		for j := 0; j < bookKeeperLen; j++ {
+			if ngbrNoders[i].GetPubKey().X.Cmp(bookKeepers[j].X) == 0 {
+				ngbType = verifyNode
+				break
+			}
+		}
+
+		ngbAddr = ngbrNoders[i].GetAddr()
+		ngbInfoPort = ngbrNoders[i].GetHttpInfoPort()
+		ngbInfoState = ngbrNoders[i].GetHttpInfoState()
+		ngbHttpInfoAddr = ngbAddr + ":" + strconv.Itoa(ngbInfoPort)
+		ngbId = fmt.Sprintf("0x%x", ngbrNoders[i].GetID())
+
+		ngbrInfo := newNgbNodeInfo(ngbId, ngbType, ngbAddr, ngbHttpInfoAddr, ngbInfoPort, ngbInfoState)
+		ngbrNodersInfo = append(ngbrNodersInfo, *ngbrInfo)
+	}
+	sort.Sort(NgbNodeInfoSlice(ngbrNodersInfo))
+
+	blockHeight := ledger.DefaultLedger.Blockchain.BlockHeight
+	pageInfo, err := initPageInfo(blockHeight, curNodeType, ngbrsLen, ngbrNodersInfo)
+	if err != nil {
+		http.Redirect(w, r, "/info", http.StatusFound)
+		return
+	}
+
+	err = templates.ExecuteTemplate(w, "info", pageInfo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func StartServer(n Noder) {
+	node = n
+	port := int(config.Parameters.HttpInfoPort)
+	http.HandleFunc("/info", viewHandler)
+	http.ListenAndServe(":" + strconv.Itoa(port), nil)
+}

net/message/version.go
@@ -1,6 +1,7 @@
 package message
 
 import (
+	"DNA/common/config"
 	"DNA/common/log"
 	"DNA/core/ledger"
 	"DNA/crypto"
@@ -13,14 +14,20 @@ import (
 	"time"
 )
 
+const (
+	HTTPINFOFLAG = 0
+)
+
 type version struct {
 	Hdr msgHdr
 	P   struct {
-		Version   uint32
-		Services  uint64
-		TimeStamp uint32
-		Port      uint16
-		Nonce     uint64
+		Version      uint32
+		Services     uint64
+		TimeStamp    uint32
+		Port         uint16
+		HttpInfoPort uint16
+		Cap          [32]byte
+		Nonce        uint64
 		// TODO remove tempory to get serilization function passed
 		UserAgent   uint8
 		StartHeight uint64
@@ -40,6 +47,13 @@ func NewVersion(n Noder) ([]byte, error) {
 
 	msg.P.Version = n.Version()
 	msg.P.Services = n.Services()
+	msg.P.HttpInfoPort = config.Parameters.HttpInfoPort
+	if config.Parameters.HttpInfoStart {
+		msg.P.Cap[HTTPINFOFLAG] = 0x01
+	} else {
+		msg.P.Cap[HTTPINFOFLAG] = 0x00
+	}
+
 	// FIXME Time overflow
 	msg.P.TimeStamp = uint32(time.Now().UTC().UnixNano())
 	msg.P.Port = n.GetPort()
@@ -172,6 +186,12 @@ func (msg version) Handle(node Noder) error {
 	}
 
 	log.Debug("handle version msg.pk is ", msg.pk)
+	if msg.P.Cap[HTTPINFOFLAG] == 0x01 {
+		node.SetHttpInfoState(true)
+	} else {
+		node.SetHttpInfoState(false)
+	}
+	node.SetHttpInfoPort(msg.P.HttpInfoPort)
 	node.SetBookKeeperAddr(msg.pk)
 	node.UpdateInfo(time.Now(), msg.P.Version, msg.P.Services,
 		msg.P.Port, msg.P.Nonce, msg.P.Relay, msg.P.StartHeight)

elastos/Elastos.ELA

main.go
@@ -12,6 +12,7 @@ import (
 	"DNA_POW/crypto"
 	"DNA_POW/net"
 	"DNA_POW/net/httpjsonrpc"
+	"DNA_POW/net/httpnodeinfo"
 	"DNA_POW/net/httprestful"
 	"DNA_POW/net/httpwebsocket"
 	"DNA_POW/net/protocol"
@@ -147,6 +148,9 @@ func main() {
 	go httpjsonrpc.StartLocalServer()
 	go httprestful.StartServer(noder)
 	go httpwebsocket.StartServer(noder)
+	if config.Parameters.HttpInfoStart {
+		go httpnodeinfo.StartServer(noder)
+	}
 	select {}
 ERROR:
 	os.Exit(1)

net/httpnodeinfo/server.go
@@ -1,0 +1,113 @@
+package httpnodeinfo
+
+import (
+	"DNA_POW/common/config"
+	"DNA_POW/core/ledger"
+	. "DNA_POW/net/protocol"
+	"fmt"
+	"html/template"
+	"net/http"
+	"sort"
+	"strconv"
+)
+
+type Info struct {
+	NodeVersion   string
+	BlockHeight   uint32
+	NeighborCnt   int
+	Neighbors     []NgbNodeInfo
+	HttpRestPort  int
+	HttpWsPort    int
+	HttpJsonPort  int
+	HttpLocalPort int
+	NodePort      int
+	NodeId        string
+	NodeType      string
+}
+
+const (
+	verifyNode  = "Verify Node"
+	serviceNode = "Service Node"
+)
+
+var node Noder
+
+var templates = template.Must(template.New("info").Parse(page))
+
+func newNgbNodeInfo(ngbId string, ngbType string, ngbAddr string, httpInfoAddr string, httpInfoPort int, httpInfoStart bool) *NgbNodeInfo {
+	return &NgbNodeInfo{NgbId: ngbId, NgbType: ngbType, NgbAddr: ngbAddr, HttpInfoAddr: httpInfoAddr,
+		HttpInfoPort: httpInfoPort, HttpInfoStart: httpInfoStart}
+}
+
+func initPageInfo(blockHeight uint32, curNodeType string, ngbrCnt int, ngbrsInfo []NgbNodeInfo) (*Info, error) {
+	id := fmt.Sprintf("0x%x", node.GetID())
+	return &Info{NodeVersion: config.Version, BlockHeight: blockHeight,
+		NeighborCnt: ngbrCnt, Neighbors: ngbrsInfo,
+		HttpRestPort:  config.Parameters.HttpRestPort,
+		HttpWsPort:    config.Parameters.HttpWsPort,
+		HttpJsonPort:  config.Parameters.HttpJsonPort,
+		HttpLocalPort: config.Parameters.HttpLocalPort,
+		NodePort:      config.Parameters.NodePort,
+		NodeId:        id, NodeType: curNodeType}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	var ngbrNodersInfo []NgbNodeInfo
+	var ngbId string
+	var ngbAddr string
+	var ngbType string
+	var ngbInfoPort int
+	var ngbInfoState bool
+	var ngbHttpInfoAddr string
+
+	curNodeType := serviceNode
+	bookKeepers, _, _ := ledger.DefaultLedger.Store.GetBookKeeperList()
+	bookKeeperLen := len(bookKeepers)
+	for i := 0; i < bookKeeperLen; i++ {
+		if node.GetPubKey().X.Cmp(bookKeepers[i].X) == 0 {
+			curNodeType = verifyNode
+			break
+		}
+	}
+
+	ngbrNoders := node.GetNeighborNoder()
+	ngbrsLen := len(ngbrNoders)
+	for i := 0; i < ngbrsLen; i++ {
+		ngbType = serviceNode
+		for j := 0; j < bookKeeperLen; j++ {
+			if ngbrNoders[i].GetPubKey().X.Cmp(bookKeepers[j].X) == 0 {
+				ngbType = verifyNode
+				break
+			}
+		}
+
+		ngbAddr = ngbrNoders[i].GetAddr()
+		ngbInfoPort = ngbrNoders[i].GetHttpInfoPort()
+		ngbInfoState = ngbrNoders[i].GetHttpInfoState()
+		ngbHttpInfoAddr = ngbAddr + ":" + strconv.Itoa(ngbInfoPort)
+		ngbId = fmt.Sprintf("0x%x", ngbrNoders[i].GetID())
+
+		ngbrInfo := newNgbNodeInfo(ngbId, ngbType, ngbAddr, ngbHttpInfoAddr, ngbInfoPort, ngbInfoState)
+		ngbrNodersInfo = append(ngbrNodersInfo, *ngbrInfo)
+	}
+	sort.Sort(NgbNodeInfoSlice(ngbrNodersInfo))
+
+	blockHeight := ledger.DefaultLedger.Blockchain.BlockHeight
+	pageInfo, err := initPageInfo(blockHeight, curNodeType, ngbrsLen, ngbrNodersInfo)
+	if err != nil {
+		http.Redirect(w, r, "/info", http.StatusFound)
+		return
+	}
+
+	err = templates.ExecuteTemplate(w, "info", pageInfo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func StartServer(n Noder) {
+	node = n
+	port := int(config.Parameters.HttpInfoPort)
+	http.HandleFunc("/info", viewHandler)
+	http.ListenAndServe(":"+strconv.Itoa(port), nil)
+}

net/message/version.go
@@ -1,6 +1,7 @@
 package message
 
 import (
+	"DNA_POW/common/config"
 	"DNA_POW/common/log"
 	"DNA_POW/core/ledger"
 	"DNA_POW/crypto"
@@ -13,14 +14,20 @@ import (
 	"time"
 )
 
+const (
+	HTTPINFOFLAG = 0
+)
+
 type version struct {
 	Hdr msgHdr
 	P   struct {
-		Version   uint32
-		Services  uint64
-		TimeStamp uint32
-		Port      uint16
-		Nonce     uint64
+		Version      uint32
+		Services     uint64
+		TimeStamp    uint32
+		Port         uint16
+		HttpInfoPort uint16
+		Cap          [32]byte
+		Nonce        uint64
 		// TODO remove tempory to get serilization function passed
 		UserAgent   uint8
 		StartHeight uint64
@@ -40,6 +47,13 @@ func NewVersion(n Noder) ([]byte, error) {
 
 	msg.P.Version = n.Version()
 	msg.P.Services = n.Services()
+	msg.P.HttpInfoPort = config.Parameters.HttpInfoPort
+	if config.Parameters.HttpInfoStart {
+		msg.P.Cap[HTTPINFOFLAG] = 0x01
+	} else {
+		msg.P.Cap[HTTPINFOFLAG] = 0x00
+	}
+
 	// FIXME Time overflow
 	msg.P.TimeStamp = uint32(time.Now().UTC().UnixNano())
 	msg.P.Port = n.GetPort()
@@ -172,6 +186,12 @@ func (msg version) Handle(node Noder) error {
 	}
 
 	log.Debug("handle version msg.pk is ", msg.pk)
+	if msg.P.Cap[HTTPINFOFLAG] == 0x01 {
+		node.SetHttpInfoState(true)
+	} else {
+		node.SetHttpInfoState(false)
+	}
+	node.SetHttpInfoPort(msg.P.HttpInfoPort)
 	node.SetBookKeeperAddr(msg.pk)
 	node.UpdateInfo(time.Now(), msg.P.Version, msg.P.Services,
 		msg.P.Port, msg.P.Nonce, msg.P.Relay, msg.P.StartHeight)
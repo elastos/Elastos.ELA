barbapapait/ft-blockchain

Changes for wallet
Signed-off-by: Ziyan Zhou <zhouziyan@hotmail.com>

account/coin.go
@@ -1,0 +1,47 @@
+package account
+
+import (
+	"DNA/core/transaction"
+	"io"
+)
+
+type CoinState byte
+
+//const (
+//	Unconfirmed     CoinState = 0
+//	Unspent         CoinState = 1
+//	Spending        CoinState = 2
+//	Spent           CoinState = 3
+//	SpentAndClaimed CoinState = 4
+//)
+
+type Coin struct {
+	//input  *transaction.UTXOTxInput
+	Output *transaction.TxOutput
+	//CoinState
+}
+
+func (coin *Coin) Serialize(w io.Writer) error {
+	//coin.input.Serialize(w)
+	coin.Output.Serialize(w)
+	//w.Write([]byte{byte(coin.CoinState)})
+	return nil
+}
+
+func (coin *Coin) Deserialize(r io.Reader) error {
+	//coin.input = new(transaction.UTXOTxInput)
+	//if err := coin.input.Deserialize(r); err != nil {
+	//	return err
+	//}
+
+	coin.Output = new(transaction.TxOutput)
+	coin.Output.Deserialize(r)
+	//
+	//state, err := serialization.ReadUint8(r)
+	//if err != nil {
+	//	return err
+	//}
+	//coin.CoinState = CoinState(state)
+
+	return nil
+}

net/httpjsonrpc/localServer.go
@@ -1,10 +1,11 @@
 package httpjsonrpc
 
 import (
-	"DNA/common/log"
-	. "DNA/common/config"
 	"net/http"
 	"strconv"
+
+	. "DNA/common/config"
+	"DNA/common/log"
 )
 
 const (
@@ -22,6 +23,19 @@ func StartLocalServer() {
 	HandleFunc("stopconsensus", stopConsensus)
 	HandleFunc("sendsampletransaction", sendSampleTransaction)
 	HandleFunc("setdebuginfo", setDebugInfo)
+
+	HandleFunc("searchtransactions", searchTransactions)
+	HandleFunc("createwallet", createWallet)
+	HandleFunc("openwallet", openWallet)
+	HandleFunc("closewallet", closeWallet)
+	HandleFunc("recoverwallet", recoverWallet)
+	HandleFunc("getwalletkey", getWalletKey)
+	HandleFunc("makeregtxn", makeRegTxn)
+	HandleFunc("makeissuetxn", makeIssueTxn)
+	HandleFunc("maketransfertxn", makeTransferTxn)
+	HandleFunc("addaccount", addAccount)
+	HandleFunc("deleteaccount", deleteAccount)
+	HandleFunc("getbalance", getBalance)
 
 	// TODO: only listen to local host
 	err := http.ListenAndServe(":"+strconv.Itoa(Parameters.HttpLocalPort), nil)

sdk/sdk.go
@@ -1,0 +1,159 @@
+package sdk
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+
+	"DNA/account"
+	. "DNA/common"
+	. "DNA/core/asset"
+	"DNA/core/contract"
+	"DNA/core/signature"
+	"DNA/core/transaction"
+	"errors"
+)
+
+const (
+	REFERTXHASHLEN = 64
+)
+
+func MakeRegTransaction(wallet account.Client, name string, value Fixed64) (*transaction.Transaction, error) {
+	admin, err := wallet.GetDefaultAccount()
+	if err != nil {
+		return nil, err
+	}
+	issuer := admin
+	asset := &Asset{name, "description", byte(MaxPrecision), AssetType(Share), UTXO}
+	transactionContract, err := contract.CreateSignatureContract(admin.PubKey())
+	if err != nil {
+		fmt.Println("CreateSignatureContract failed")
+		return nil, err
+	}
+	tx, _ := transaction.NewRegisterAssetTransaction(asset, value, issuer.PubKey(), transactionContract.ProgramHash)
+	txAttr := transaction.NewTxAttribute(transaction.Nonce, []byte(strconv.FormatInt(rand.Int63(), 10)))
+	tx.Attributes = make([]*transaction.TxAttribute, 0)
+	tx.Attributes = append(tx.Attributes, &txAttr)
+	if err := signTransaction(issuer, tx); err != nil {
+		fmt.Println("sign regist transaction failed")
+		return nil, err
+	}
+
+	return tx, nil
+}
+
+func MakeIssueTransaction(wallet account.Client, assetID Uint256, programHash Uint160, value Fixed64) (*transaction.Transaction, error) {
+	admin, err := wallet.GetDefaultAccount()
+	if err != nil {
+		return nil, err
+	}
+
+	issueTxOutput := &transaction.TxOutput{
+		AssetID:     assetID,
+		Value:       value,
+		ProgramHash: programHash,
+	}
+	outputs := []*transaction.TxOutput{issueTxOutput}
+	tx, _ := transaction.NewIssueAssetTransaction(outputs)
+	txAttr := transaction.NewTxAttribute(transaction.Nonce, []byte(strconv.FormatInt(rand.Int63(), 10)))
+	tx.Attributes = make([]*transaction.TxAttribute, 0)
+	tx.Attributes = append(tx.Attributes, &txAttr)
+	if err := signTransaction(admin, tx); err != nil {
+		fmt.Println("sign issue transaction failed")
+		return nil, err
+	}
+	return tx, nil
+}
+
+func MakeTransferTransaction(wallet account.Client, assetID Uint256, programhash Uint160, value Fixed64) (*transaction.Transaction, error) {
+	mainAccount, err := wallet.GetDefaultAccount()
+	if err != nil {
+		return nil, err
+	}
+	coins := wallet.GetCoins()
+	input := []*transaction.UTXOTxInput{}
+	output := []*transaction.TxOutput{}
+	expected := value
+	for ref, coin := range coins {
+		if coin.Output.ProgramHash == programhash {
+			input = append(input, ref)
+			if coin.Output.Value > expected {
+				transfered := transaction.TxOutput{
+					AssetID:     assetID,
+					Value:       expected,
+					ProgramHash: programhash,
+				}
+				getChanged := transaction.TxOutput{
+					AssetID:     assetID,
+					Value:       coin.Output.Value - expected,
+					ProgramHash: mainAccount.ProgramHash,
+				}
+				output = append(output, &transfered, &getChanged)
+				expected = 0
+				break
+			} else if coin.Output.Value == expected {
+				transfered := transaction.TxOutput{
+					AssetID:     assetID,
+					Value:       expected,
+					ProgramHash: programhash,
+				}
+				output = append(output, &transfered)
+				expected = 0
+				break
+			} else if coin.Output.Value < expected {
+				transfered := transaction.TxOutput{
+					AssetID:     assetID,
+					Value:       coin.Output.Value,
+					ProgramHash: programhash,
+				}
+				output = append(output, &transfered)
+				expected = expected - coin.Output.Value
+			}
+		}
+	}
+	if expected > 0 {
+		return nil, errors.New("Token is not enough")
+	}
+
+	txn, err := transaction.NewTransferAssetTransaction(input, output)
+	if err != nil {
+		return nil, err
+	}
+	txAttr := transaction.NewTxAttribute(transaction.Nonce, []byte(strconv.FormatInt(rand.Int63(), 10)))
+	txn.Attributes = make([]*transaction.TxAttribute, 0)
+	txn.Attributes = append(txn.Attributes, &txAttr)
+
+	if err := signTransaction(mainAccount, txn); err != nil {
+		fmt.Println("sign transfer transaction failed")
+		return nil, err
+	}
+	return txn, nil
+}
+
+func signTransaction(signer *account.Account, tx *transaction.Transaction) error {
+	signature, err := signature.SignBySigner(tx, signer)
+	if err != nil {
+		fmt.Println("SignBySigner failed")
+		return err
+	}
+	transactionContract, err := contract.CreateSignatureContract(signer.PubKey())
+	if err != nil {
+		fmt.Println("CreateSignatureContract failed")
+		return err
+	}
+	transactionContractContext := newContractContextWithoutProgramHashes(tx)
+	if err := transactionContractContext.AddContract(transactionContract, signer.PubKey(), signature); err != nil {
+		fmt.Println("SaveContract failed")
+		return err
+	}
+	tx.SetPrograms(transactionContractContext.GetPrograms())
+	return nil
+}
+
+func newContractContextWithoutProgramHashes(data signature.SignableData) *contract.ContractContext {
+	return &contract.ContractContext{
+		Data:       data,
+		Codes:      make([][]byte, 1),
+		Parameters: make([][][]byte, 1),
+	}
+}

elastos/Elastos.ELA

account/coin.go
@@ -1,0 +1,47 @@
+package account
+
+import (
+	"DNA_POW/core/transaction"
+	"io"
+)
+
+type CoinState byte
+
+//const (
+//	Unconfirmed     CoinState = 0
+//	Unspent         CoinState = 1
+//	Spending        CoinState = 2
+//	Spent           CoinState = 3
+//	SpentAndClaimed CoinState = 4
+//)
+
+type Coin struct {
+	//input  *transaction.UTXOTxInput
+	Output *transaction.TxOutput
+	//CoinState
+}
+
+func (coin *Coin) Serialize(w io.Writer) error {
+	//coin.input.Serialize(w)
+	coin.Output.Serialize(w)
+	//w.Write([]byte{byte(coin.CoinState)})
+	return nil
+}
+
+func (coin *Coin) Deserialize(r io.Reader) error {
+	//coin.input = new(transaction.UTXOTxInput)
+	//if err := coin.input.Deserialize(r); err != nil {
+	//	return err
+	//}
+
+	coin.Output = new(transaction.TxOutput)
+	coin.Output.Deserialize(r)
+	//
+	//state, err := serialization.ReadUint8(r)
+	//if err != nil {
+	//	return err
+	//}
+	//coin.CoinState = CoinState(state)
+
+	return nil
+}

net/httpjsonrpc/localServer.go
@@ -1,10 +1,11 @@
 package httpjsonrpc
 
 import (
-	"DNA_POW/common/log"
-	. "DNA_POW/common/config"
 	"net/http"
 	"strconv"
+
+	. "DNA_POW/common/config"
+	"DNA_POW/common/log"
 )
 
 const (
@@ -22,6 +23,19 @@ func StartLocalServer() {
 	HandleFunc("stopconsensus", stopConsensus)
 	HandleFunc("sendsampletransaction", sendSampleTransaction)
 	HandleFunc("setdebuginfo", setDebugInfo)
+
+	HandleFunc("searchtransactions", searchTransactions)
+	HandleFunc("createwallet", createWallet)
+	HandleFunc("openwallet", openWallet)
+	HandleFunc("closewallet", closeWallet)
+	HandleFunc("recoverwallet", recoverWallet)
+	HandleFunc("getwalletkey", getWalletKey)
+	HandleFunc("makeregtxn", makeRegTxn)
+	HandleFunc("makeissuetxn", makeIssueTxn)
+	HandleFunc("maketransfertxn", makeTransferTxn)
+	HandleFunc("addaccount", addAccount)
+	HandleFunc("deleteaccount", deleteAccount)
+	HandleFunc("getbalance", getBalance)
 
 	// TODO: only listen to local host
 	err := http.ListenAndServe(":"+strconv.Itoa(Parameters.HttpLocalPort), nil)

sdk/sdk.go
@@ -1,0 +1,159 @@
+package sdk
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+
+	"DNA_POW/account"
+	. "DNA_POW/common"
+	. "DNA_POW/core/asset"
+	"DNA_POW/core/contract"
+	"DNA_POW/core/signature"
+	"DNA_POW/core/transaction"
+	"errors"
+)
+
+const (
+	REFERTXHASHLEN = 64
+)
+
+func MakeRegTransaction(wallet account.Client, name string, value Fixed64) (*transaction.Transaction, error) {
+	admin, err := wallet.GetDefaultAccount()
+	if err != nil {
+		return nil, err
+	}
+	issuer := admin
+	asset := &Asset{name, "description", byte(MaxPrecision), AssetType(Share), UTXO}
+	transactionContract, err := contract.CreateSignatureContract(admin.PubKey())
+	if err != nil {
+		fmt.Println("CreateSignatureContract failed")
+		return nil, err
+	}
+	tx, _ := transaction.NewRegisterAssetTransaction(asset, value, issuer.PubKey(), transactionContract.ProgramHash)
+	txAttr := transaction.NewTxAttribute(transaction.Nonce, []byte(strconv.FormatInt(rand.Int63(), 10)))
+	tx.Attributes = make([]*transaction.TxAttribute, 0)
+	tx.Attributes = append(tx.Attributes, &txAttr)
+	if err := signTransaction(issuer, tx); err != nil {
+		fmt.Println("sign regist transaction failed")
+		return nil, err
+	}
+
+	return tx, nil
+}
+
+func MakeIssueTransaction(wallet account.Client, assetID Uint256, programHash Uint160, value Fixed64) (*transaction.Transaction, error) {
+	admin, err := wallet.GetDefaultAccount()
+	if err != nil {
+		return nil, err
+	}
+
+	issueTxOutput := &transaction.TxOutput{
+		AssetID:     assetID,
+		Value:       value,
+		ProgramHash: programHash,
+	}
+	outputs := []*transaction.TxOutput{issueTxOutput}
+	tx, _ := transaction.NewIssueAssetTransaction(outputs)
+	txAttr := transaction.NewTxAttribute(transaction.Nonce, []byte(strconv.FormatInt(rand.Int63(), 10)))
+	tx.Attributes = make([]*transaction.TxAttribute, 0)
+	tx.Attributes = append(tx.Attributes, &txAttr)
+	if err := signTransaction(admin, tx); err != nil {
+		fmt.Println("sign issue transaction failed")
+		return nil, err
+	}
+	return tx, nil
+}
+
+func MakeTransferTransaction(wallet account.Client, assetID Uint256, programhash Uint160, value Fixed64) (*transaction.Transaction, error) {
+	mainAccount, err := wallet.GetDefaultAccount()
+	if err != nil {
+		return nil, err
+	}
+	coins := wallet.GetCoins()
+	input := []*transaction.UTXOTxInput{}
+	output := []*transaction.TxOutput{}
+	expected := value
+	for ref, coin := range coins {
+		if coin.Output.ProgramHash == programhash {
+			input = append(input, ref)
+			if coin.Output.Value > expected {
+				transfered := transaction.TxOutput{
+					AssetID:     assetID,
+					Value:       expected,
+					ProgramHash: programhash,
+				}
+				getChanged := transaction.TxOutput{
+					AssetID:     assetID,
+					Value:       coin.Output.Value - expected,
+					ProgramHash: mainAccount.ProgramHash,
+				}
+				output = append(output, &transfered, &getChanged)
+				expected = 0
+				break
+			} else if coin.Output.Value == expected {
+				transfered := transaction.TxOutput{
+					AssetID:     assetID,
+					Value:       expected,
+					ProgramHash: programhash,
+				}
+				output = append(output, &transfered)
+				expected = 0
+				break
+			} else if coin.Output.Value < expected {
+				transfered := transaction.TxOutput{
+					AssetID:     assetID,
+					Value:       coin.Output.Value,
+					ProgramHash: programhash,
+				}
+				output = append(output, &transfered)
+				expected = expected - coin.Output.Value
+			}
+		}
+	}
+	if expected > 0 {
+		return nil, errors.New("Token is not enough")
+	}
+
+	txn, err := transaction.NewTransferAssetTransaction(input, output)
+	if err != nil {
+		return nil, err
+	}
+	txAttr := transaction.NewTxAttribute(transaction.Nonce, []byte(strconv.FormatInt(rand.Int63(), 10)))
+	txn.Attributes = make([]*transaction.TxAttribute, 0)
+	txn.Attributes = append(txn.Attributes, &txAttr)
+
+	if err := signTransaction(mainAccount, txn); err != nil {
+		fmt.Println("sign transfer transaction failed")
+		return nil, err
+	}
+	return txn, nil
+}
+
+func signTransaction(signer *account.Account, tx *transaction.Transaction) error {
+	signature, err := signature.SignBySigner(tx, signer)
+	if err != nil {
+		fmt.Println("SignBySigner failed")
+		return err
+	}
+	transactionContract, err := contract.CreateSignatureContract(signer.PubKey())
+	if err != nil {
+		fmt.Println("CreateSignatureContract failed")
+		return err
+	}
+	transactionContractContext := newContractContextWithoutProgramHashes(tx)
+	if err := transactionContractContext.AddContract(transactionContract, signer.PubKey(), signature); err != nil {
+		fmt.Println("SaveContract failed")
+		return err
+	}
+	tx.SetPrograms(transactionContractContext.GetPrograms())
+	return nil
+}
+
+func newContractContextWithoutProgramHashes(data signature.SignableData) *contract.ContractContext {
+	return &contract.ContractContext{
+		Data:       data,
+		Codes:      make([][]byte, 1),
+		Parameters: make([][][]byte, 1),
+	}
+}
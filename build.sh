90cea014ac42ba841997bb96dbba9f050ee7af64
Li Bin <libin@elastos.org>
2017-12-15 22:09:04 +0800

    build script: add support for macosx 10.12.6

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 #
 # Utility
 #
-install_package()
+install_package_ubuntu()
 {
     local PKG_NAME=$1
     local PPA_NAME=$2
@@ -27,44 +27,13 @@ install_package()
     sudo apt-get install -y $PKG_NAME
 }
 
-#
-# Development Environment: Directory Layout
-#
-# $HOME
-#  dev/         - DEV_ROOT - GOPATH
-#    src/       - SRC_ROOT
-#      DNA_POW/ - SRC_PATH
-#        deps/  - Depended C source code
-#
-setenv()
+install_source_ubuntu()
 {
-    install_package git
-    install_package software-properties-common
-    install_package golang-1.8-go ppa:gophers/archive
-    install_package glide ppa:masterminds/glide
-
-    export SCRIPT_PATH=$(cd $(dirname $BASH_SOURCE); pwd)
-    export SRC_PATH=$SCRIPT_PATH
-
-    export DEV_ROOT=$(cd $SCRIPT_PATH/../..; pwd)
-    export SRC_ROOT=$DEV_ROOT/src
-
-    export GOROOT=/usr/lib/go-1.8
-    export GOPATH=$DEV_ROOT
-
-    export PATH=$GOROOT/bin:$PATH
-    export PATH=$GOBIN:$PATH
-
-    NCPU=1
-    if [ "$(uname -s)" == "Linux" ]; then
-        NCPU=$(($(grep '^processor' /proc/cpuinfo | wc -l) * 2))
-    elif [ "$(uname -s)" == "Darwin" ]; then
-        NCPU=$(($(sysctl -n hw.ncpu) * 2))
+    if [ -f "/usr/local/lib/libzmq.so.5.1.2" ]; then
+        echo "INFO: /usr/local/lib/libzmq.so.5.1.2 exist."
+        return
     fi
-}
 
-build_dependencies()
-{
     mkdir -p $SRC_PATH/deps/
     cd $SRC_PATH/deps/
 
@@ -82,11 +51,105 @@ build_dependencies()
     sudo ldconfig
 }
 
+install_package_macosx()
+{
+    local PKG_NAME=$1
+    local BREW_INSTALL_OPTION=$2
+
+    if [ "$PKG_NAME" == "" ]; then
+        echo "ERROR: no package name specified"
+        return
+    fi
+
+    which brew 1>/dev/null 2>/dev/null
+    if [ "$?" != "0" ]; then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+
+    brew list $PKG_NAME 1>/dev/null 2>/dev/null
+    if [ "$?" == "0" ]; then
+        return
+    fi
+    brew install $BREW_INSTALL_OPTION $PKG_NAME
+}
+
+#
+# Development Environment: Directory Layout
+#
+# $HOME
+#  dev/         - DEV_ROOT - GOPATH
+#    src/       - SRC_ROOT
+#      DNA_POW/ - SRC_PATH
+#        deps/  - Depended C source code
+#
+setenv()
+{
+    #
+    # Check OS version
+    #
+    OS_NAME=$(uname -s)
+    if [ "$OS_NAME" == "Linux" ]; then
+        OS_DETAIL="$(lsb_release -i -s) $(lsb_release -r -s) $(uname -m)"
+    elif [ "$OS_NAME" == "Darwin" ]; then
+        OS_DETAIL="$(uname -srm)"
+    fi
+
+    if [ "$OS_DETAIL" == "Ubuntu 16.04 x86_64" ]; then
+        echo "$OS_DETAIL: Supported"
+    elif [ "$OS_DETAIL" == "Darwin 16.7.0 x86_64" ]; then
+        echo "$OS_DETAIL: Supported"
+    else
+        echo "ERROR: $OS_DETAIL have not been tested"
+        exit
+    fi
+
+    if [ "$OS_NAME" == "Linux" ]; then
+        install_package_ubuntu git
+        install_package_ubuntu software-properties-common
+        install_package_ubuntu golang-1.8-go ppa:gophers/archive
+        install_package_ubuntu glide ppa:masterminds/glide
+    elif [ "$OS_NAME" == "Darwin" ]; then
+        install_package_macosx go@1.8
+        install_package_macosx glide --ignore-dependencies
+        install_package_macosx pkg-config
+        install_package_macosx zeromq@4.2
+    fi
+
+    export SCRIPT_PATH=$(cd $(dirname $BASH_SOURCE); pwd)
+    export SRC_PATH=$SCRIPT_PATH
+
+    export DEV_ROOT=$(cd $SCRIPT_PATH/../..; pwd)
+    export SRC_ROOT=$DEV_ROOT/src
+
+    if [ "$OS_NAME" == "Linux" ]; then
+        export GOROOT=/usr/lib/go-1.8
+    elif [ "$OS_NAME" == "Darwin" ]; then
+        export GOROOT=/usr/local/opt/go@1.8/libexec
+    fi
+
+    export GOPATH=$DEV_ROOT
+    export PATH=$GOROOT/bin:$PATH
+
+    go version
+    glide --version
+
+    NCPU=1
+    if [ "$(uname -s)" == "Linux" ]; then
+        NCPU=$(($(grep '^processor' /proc/cpuinfo | wc -l) * 2))
+    elif [ "$(uname -s)" == "Darwin" ]; then
+        NCPU=$(($(sysctl -n hw.ncpu) * 2))
+    fi
+
+    if [ "$OS_NAME" == "Linux" ]; then
+        install_source_ubuntu
+    fi
+}
+
 build()
 {
     cd $SRC_PATH
     glide install
-    make
+    make $*
 }
 
 usage()
@@ -98,29 +161,10 @@ usage()
 #
 # Main
 #
-
-#
-# Check OS version
-#
-OS_NAME=$(uname -s)
-if [ "$OS_NAME" == "Linux" ]; then
-    OS_DETAIL="$(lsb_release -i -s) $(lsb_release -r -s) $(uname -m)"
-elif [ "$OS_NAME" == "Darwin" ]; then
-    OS_DETAIL="$(uname -srm)"
-fi
-
-if [ "$OS_DETAIL" == "Ubuntu 16.04 x86_64" ]; then
-    echo "$OS_DETAIL: Supported"
-else
-    echo "ERROR: $OS_DETAIL have not been tested"
-    exit
-fi
-
 if [ "$1" == "-h" ]; then
     usage
     exit
 fi
 
 setenv
-build_dependencies
-build
+build $1

d26c284bb1122e8e3ea35488c0c3ad74d0e5bb58
hejianhui <hejianhui@elastos.org>
2017-12-19 20:06:15 +0800

    s

--- core/store/ChainStore/ChainStore.go
+++ core/store/ChainStore/ChainStore.go
@@ -68,17 +68,7 @@ type ChainStore struct {
 
 func NewLedgerStore() (ILedgerStore, error) {
 	// TODO: read config file decide which db to use.
-	cs, err := NewChainStore("Chain")
-	if err != nil {
-		return nil, err
-	}
-
-	return cs, nil
-}
-
-func NewChainStore(file string) (*ChainStore, error) {
-
-	st, err := NewStore(file)
+	st, err := NewLevelDBStore("Chain")
 	if err != nil {
 		return nil, err
 	}
@@ -97,13 +87,11 @@ func NewChainStore(file string) (*ChainStore, error) {
 
 	go chain.loop()
 
+	if err != nil {
+		return nil, err
+	}
+
 	return chain, nil
-}
-
-func NewStore(file string) (IStore, error) {
-	ldbs, err := NewLevelDBStore(file)
-
-	return ldbs, err
 }
 
 func (self *ChainStore) Close() {

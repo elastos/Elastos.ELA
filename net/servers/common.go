7257007af1378cf008fdecd18c5b6e57cea78adc
hejianhui <hejianhui@elastos.org>
2018-01-10 20:16:23 +0800

    add default value to response result value

--- net/servers/common.go
+++ net/servers/common.go
@@ -158,5 +158,8 @@ func VerifyAndSendTx(txn *tx.Transaction) ErrCode {
 }
 
 func ResponsePack(errCode ErrCode, result interface{}) map[string]interface{} {
+	if errCode != 0 && (result == "" || result == nil) {
+		result = ErrMap[errCode]
+	}
 	return map[string]interface{}{"Result": result, "Error": errCode}
 }

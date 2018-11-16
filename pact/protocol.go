847e8046c6e141fbcaa783730358bb856e7ecfa5
AlexPan <ppgod@live.cn>
2018-11-16 17:22:40 +0800

    include pact/ and peer/ package to start P2P protocol refactor

---
+++ pact/protocol.go
@@ -0,0 +1,68 @@
+package pact
+
+import (
+	"strconv"
+	"strings"
+)
+
+// Release version numbers
+const (
+	// EBIP002Version is the protocol version which included DPos consensus
+	// protocol.
+	EBIP002Version uint32 = 10002
+)
+
+// ServiceFlag identifies services supported by a peer.
+type ServiceFlag uint64
+
+const (
+	// SFNodeNetwork is a flag used to indicate a peer is a full node.
+	SFNodeNetwork ServiceFlag = 1 << iota
+
+	// SFTxFilters is a flag used to indicate a peer supports transaction
+	// filtering.
+	SFTxFilters
+
+	// SFNodeOpen is a flag used to indicate a peer supports open service.
+	SFNodeOpen
+)
+
+// Map of service flags back to their constant names for pretty printing.
+var sfStrings = map[ServiceFlag]string{
+	SFNodeNetwork: "SFNodeNetwork",
+	SFTxFilters:   "SFTxFilters",
+	SFNodeOpen:    "SFNodeOpen",
+}
+
+// orderedSFStrings is an ordered list of service flags from highest to
+// lowest.
+var orderedSFStrings = []ServiceFlag{
+	SFNodeNetwork,
+	SFTxFilters,
+	SFNodeOpen,
+}
+
+// String returns the ServiceFlag in human-readable form.
+func (f ServiceFlag) String() string {
+	// No flags are set.
+	if f == 0 {
+		return "0x0"
+	}
+
+	// Add individual bit flags.
+	s := ""
+	for _, flag := range orderedSFStrings {
+		if f&flag == flag {
+			s += sfStrings[flag] + "|"
+			f -= flag
+		}
+	}
+
+	// Add any remaining flags which aren't accounted for as hex.
+	s = strings.TrimRight(s, "|")
+	if f != 0 {
+		s += "|0x" + strconv.FormatUint(uint64(f), 16)
+	}
+	s = strings.TrimLeft(s, "|")
+	return s
+}

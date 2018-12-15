5e9b5336732868a01cae1951cff3ad6dc375efbc
Yan Minzhi <yanmingzhi@elastos.org>
2018-12-15 10:58:48 +0800

    Add tiny test framework for lua.

--- test/white_box/main/test_all.lua
+++ test/white_box/main/test_all.lua
@@ -1,15 +1,38 @@
 local api = require("api")
+local colors = require 'test/common/ansicolors'
 
 local function do_files(inputstr, sep, base_path)
     if sep == nil then
         sep = "%s"
     end
+
+    local file_count = 1
+    local test_files = {}
     for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
-        dofile(base_path .. str)
+        test_files[file_count] = base_path .. str
+        file_count = file_count + 1
     end
+
+    local result = 0
+    for i = 1, file_count - 1 do
+        local temp = dofile(test_files[i])
+
+        if temp == nil then
+            result = result + 1
+        else
+            result = result + temp
+        end
+    end
+
+    if result ~= 0 then
+        print(colors("%{red} White box tests in " .. base_path .. "failed, failed numbers :" .. result))
+    end
+
+    return result
 end
 
 local dpos_dir = "test/white_box/dpos/"
 local dpos_files = api.get_dir_all_files(dpos_dir)
-do_files(dpos_files, ",", dpos_dir)
+local result = do_files(dpos_files, ",", dpos_dir)
 
+return result

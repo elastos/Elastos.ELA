89a665b789e2f7aa254dcd85fb73242b504228a1
jiangzehua <1092431698@qq.com>
2021-01-07 17:24:59 +0800

    Fixed an issue where the Gorutine ID was incorrectly obtained

--- common/common.go
+++ common/common.go
@@ -97,7 +97,7 @@ func Hash(data []byte) Uint256 {
 
 // Goid returns the current goroutine id.
 func Goid() string {
-	var buf [18]byte
+	var buf [32]byte
 	n := runtime.Stack(buf[:], false)
 	fields := strings.Fields(string(buf[:n]))
 	if len(fields) <= 1 {

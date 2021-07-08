0ebe13a05a26e3926d17662370cb204bf8e16ac5
xiaoming <1406629722@qq.com>
2021-07-08 12:25:56 +0800

    Fix

--- cmd/script/api/payloadtype.go
+++ cmd/script/api/payloadtype.go
@@ -1415,8 +1415,8 @@ func newCRCRegisterSideChainProposalHash(L *lua.LState) int {
 	genesisHashStr := L.ToString(7)
 	genesisTimestamp := L.ToInt64(8)
 	genesisBlockDifficulty := L.ToString(9)
-	upgradeProposalType := L.ToInt64(10)
-	draftHashStr := L.ToString(11)
+	draftHashStr := L.ToString(10)
+	upgradeProposalType := L.ToInt64(11)
 
 	needSign := true
 	client, err := checkClient(L, 12)

64f41fa034bbab8a8a0735e88cadd38799899199
xiaoming <1406629722@qq.com>
2021-10-22 10:41:17 +0800

    feat(dpos2.0): Add dposV2 lua && cli gen dposV2 address

--- core/contract/contract.go
+++ core/contract/contract.go
@@ -17,8 +17,7 @@ const (
 	PrefixCrossChain PrefixType = 0x4B
 	PrefixDeposit    PrefixType = 0x1F
 	PrefixCRDID      PrefixType = 0x67
-	//TODO modify the value of PrefixDposV2 's'
-	PrefixDposV2 PrefixType = 0x99
+	PrefixDposV2     PrefixType = 0x3f
 )
 
 // Contract include the redeem script and hash prefix

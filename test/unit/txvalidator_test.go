6c7e97ce7a2ba6ced84c80f4eb9c08bbaa1e2725
xiaoming <1406629722@qq.com>
2022-04-17 21:15:07 +0800

    feat(dpos2.0): add claimdposv2 transaction testcase

--- test/unit/txvalidator_test.go
+++ test/unit/txvalidator_test.go
@@ -5123,6 +5123,78 @@ func (s *txValidatorTestSuite) TestCreateCRCAppropriationTransaction() {
 	s.NotNil(txCrcAppropriation)
 }
 
+func (s *txValidatorTestSuite) TestCreateCRClaimDposV2Transaction() {
+	buf := new(bytes.Buffer)
+	apPayload := &payload.DposV2ClaimReward{
+		Amount: common.Fixed64(100000000),
+	}
+	publicKeyStr1 := "02ca89a5fe6213da1b51046733529a84f0265abac59005f6c16f62330d20f02aeb"
+	publicKey1, _ := common.HexStringToBytes(publicKeyStr1)
+	privateKeyStr1 := "7a50d2b036d64fcb3d344cee429f61c4a3285a934c45582b26e8c9227bc1f33a"
+	privateKey1, _ := common.HexStringToBytes(privateKeyStr1)
+
+	apPayload.SerializeUnsigned(buf, payload.ActivateProducerVersion)
+	signature, _ := crypto.Sign(privateKey1, buf.Bytes())
+	apPayload.Signature = signature
+	pk, _ := crypto.DecodePoint(publicKey1)
+	redeemScript, _ := contract.CreateStandardRedeemScript(pk)
+
+	// create program
+	var txProgram = &program.Program{
+		Code:      redeemScript,
+		Parameter: nil,
+	}
+	txn := functions.CreateTransaction(
+		common2.TxVersion09,
+		common2.DposV2ClaimReward,
+		0,
+		apPayload,
+		nil,
+		[]*common2.Input{},
+		[]*common2.Output{},
+		0,
+		[]*program.Program{txProgram})
+	tx := txn.(*transaction.DposV2ClaimRewardTransaction)
+	tx.DefaultChecker.SetParameters(&transaction.TransactionParameters{
+		BlockChain: s.Chain,
+		Config:     s.Chain.GetParams(),
+	})
+
+	err, _ := tx.SpecialContextCheck()
+	s.EqualError(err.(errors.ELAError).InnerError(), "can not claim reward before dposv2startheight")
+
+	param := s.Chain.GetParams()
+	param.DPoSV2StartHeight = 10
+	tx.DefaultChecker.SetParameters(&transaction.TransactionParameters{
+		BlockChain:  s.Chain,
+		Config:      param,
+		BlockHeight: 100,
+	})
+	err, _ = tx.SpecialContextCheck()
+	s.EqualError(err.(errors.ELAError).InnerError(), "no reward to claim for such adress")
+
+	bc := s.Chain
+	bc.GetState().DposV2RewardInfo["ERyUmNH51roR9qfru37Kqkaok2NghR7L5U"] = 100
+	tx.DefaultChecker.SetParameters(&transaction.TransactionParameters{
+		BlockChain:  bc,
+		Config:      param,
+		BlockHeight: 100,
+	})
+
+	err, _ = tx.SpecialContextCheck()
+	s.EqualError(err.(errors.ELAError).InnerError(), "claim reward exceeded , max claim reward 0.00000100")
+
+	bc = s.Chain
+	bc.GetState().DposV2RewardInfo["ERyUmNH51roR9qfru37Kqkaok2NghR7L5U"] = 10000000000
+	tx.DefaultChecker.SetParameters(&transaction.TransactionParameters{
+		BlockChain:  bc,
+		Config:      param,
+		BlockHeight: 100,
+	})
+	err, _ = tx.SpecialContextCheck()
+	s.NoError(err)
+}
+
 func TestTxValidatorSuite(t *testing.T) {
 	suite.Run(t, new(txValidatorTestSuite))
 }

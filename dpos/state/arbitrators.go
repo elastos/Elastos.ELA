cbabe4e5c4ca237ce8590d630f56d43ce1a4af82
jiangzehua <1092431698@qq.com>
2022-08-09 16:14:56 +0800

    fix(dpos2.0): remove abnormal CR from random node list

--- dpos/state/arbitrators.go
+++ dpos/state/arbitrators.go
@@ -2002,7 +2002,7 @@ func (a *Arbiters) getSortedProducersWithRandom(height uint32, unclaimedCount in
 	return newProducers, nil
 }
 
-func (a *Arbiters) getRandomDposV2Producers(height uint32, unclaimedCount int, choosingArbiters map[common.Uint168]ArbiterMember) ([]string, error) {
+func (a *Arbiters) getRandomDposV2Producers(height uint32, unclaimedCount int, choosingCRArbiters map[common.Uint168]ArbiterMember) ([]string, error) {
 	block, _ := a.getBlockByHeight(height - 1)
 	if block == nil {
 		return nil, errors.New("block is not found")
@@ -2019,8 +2019,10 @@ func (a *Arbiters) getRandomDposV2Producers(height uint32, unclaimedCount int, c
 	votedProducers := a.getSortedProducersDposV2()
 	// crc also need to be random selected
 	var producerKeys []string
-	for _, crc := range choosingArbiters {
-		producerKeys = append(producerKeys, hex.EncodeToString(crc.GetOwnerPublicKey()))
+	for _, crc := range choosingCRArbiters {
+		if crc.IsNormal() {
+			producerKeys = append(producerKeys, hex.EncodeToString(crc.GetOwnerPublicKey()))
+		}
 	}
 	sort.Slice(producerKeys, func(i, j int) bool {
 		return strings.Compare(producerKeys[i], producerKeys[j]) < 0
@@ -2101,7 +2103,7 @@ func (a *Arbiters) UpdateNextArbitrators(versionHeight, height uint32) error {
 		})
 	}
 
-	unclaimed, choosingArbiters, err := a.resetNextArbiterByCRC(versionHeight, height)
+	unclaimed, choosingCRArbiters, err := a.resetNextArbiterByCRC(versionHeight, height)
 	if err != nil {
 		return err
 	}
@@ -2110,9 +2112,9 @@ func (a *Arbiters) UpdateNextArbitrators(versionHeight, height uint32) error {
 
 		count := a.ChainParams.GeneralArbiters
 		var votedProducers []*Producer
-		var votedProducersStr []string
+		var crAndVotedProducersStr []string
 		if a.isDposV2Active() {
-			votedProducersStr, err = a.getRandomDposV2Producers(height, unclaimed, choosingArbiters)
+			crAndVotedProducersStr, err = a.getRandomDposV2Producers(height, unclaimed, choosingCRArbiters)
 			if err != nil {
 				return err
 			}
@@ -2125,7 +2127,7 @@ func (a *Arbiters) UpdateNextArbitrators(versionHeight, height uint32) error {
 		var producers []ArbiterMember
 		var err error
 		if a.isDposV2Active() {
-			producers, err = a.GetDposV2NormalArbitratorsDesc(count+int(a.ChainParams.CRMemberCount), votedProducersStr, choosingArbiters)
+			producers, err = a.GetDposV2NormalArbitratorsDesc(count+int(a.ChainParams.CRMemberCount), crAndVotedProducersStr, choosingCRArbiters)
 		} else {
 			producers, err = a.GetNormalArbitratorsDesc(versionHeight, count,
 				votedProducers, unclaimed)
@@ -2203,7 +2205,7 @@ func (a *Arbiters) UpdateNextArbitrators(versionHeight, height uint32) error {
 					votedProducers)
 			} else {
 				candidates, err = a.GetDposV2CandidatesDesc(count+int(a.ChainParams.CRMemberCount),
-					votedProducersStr, choosingArbiters)
+					crAndVotedProducersStr, choosingCRArbiters)
 			}
 			if err != nil {
 				return err

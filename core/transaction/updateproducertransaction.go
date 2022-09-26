302716472b967e7bc9857346d8791f9b2171aba7
xiaoming <1406629722@qq.com>
2022-09-26 11:49:57 +0800

    fix(dpos2.0): remove unused code

--- core/transaction/updateproducertransaction.go
+++ core/transaction/updateproducertransaction.go
@@ -131,11 +131,6 @@ func (t *UpdateProducerTransaction) SpecialContextCheck() (elaerr.ELAError, bool
 
 		if info.StakeUntil < producer.Info().StakeUntil {
 			return elaerr.Simple(elaerr.ErrTxPayload, errors.New("stake time is smaller than before")), true
-		} else if info.StakeUntil > producer.Info().StakeUntil {
-			//new StakeUntil must bigger than BlockHeight
-			if t.parameters.BlockHeight >= info.StakeUntil {
-				return elaerr.Simple(elaerr.ErrTxPayload, errors.New("v2 producer StakeUntil less than BlockHeight")), true
-			}
 		}
 
 	case state.DPoSV1V2:

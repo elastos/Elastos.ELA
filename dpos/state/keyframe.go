ac83bf4d683047f9a18b68bdd42e2c06f8bc4ce9
jiangzehua <1092431698@qq.com>
2022-11-16 12:26:32 +0800

    fix(dpos2.0): modify to clearing DPoS reward correctly

--- dpos/state/keyframe.go
+++ dpos/state/keyframe.go
@@ -35,7 +35,6 @@ func (consesus ConsesusAlgorithm) String() string {
 // StateKeyFrame holds necessary state about State
 type StateKeyFrame struct {
 	NodeOwnerKeys            map[string]string // NodePublicKey as key, OwnerPublicKey as value
-	LastCRNodeOwnerKeys      map[string]string // NodePublicKey as key, OwnerPublicKey as value
 	CurrentCRNodeOwnerKeys   map[string]string // NodePublicKey as key, OwnerPublicKey as value
 	NextCRNodeOwnerKeys      map[string]string // NodePublicKey as key, OwnerPublicKey as value
 	PendingProducers         map[string]*Producer
@@ -102,7 +101,6 @@ type RewardData struct {
 func (s *StateKeyFrame) snapshot() *StateKeyFrame {
 	state := StateKeyFrame{
 		NodeOwnerKeys:            make(map[string]string),
-		LastCRNodeOwnerKeys:      make(map[string]string),
 		CurrentCRNodeOwnerKeys:   make(map[string]string),
 		NextCRNodeOwnerKeys:      make(map[string]string),
 		PendingProducers:         make(map[string]*Producer),
@@ -131,7 +129,6 @@ func (s *StateKeyFrame) snapshot() *StateKeyFrame {
 		ProducerDepositMap:       make(map[common.Uint168]struct{}),
 	}
 	state.NodeOwnerKeys = copyStringMap(s.NodeOwnerKeys)
-	state.LastCRNodeOwnerKeys = copyStringMap(s.LastCRNodeOwnerKeys)
 	state.CurrentCRNodeOwnerKeys = copyStringMap(s.CurrentCRNodeOwnerKeys)
 	state.NextCRNodeOwnerKeys = copyStringMap(s.NextCRNodeOwnerKeys)
 	state.PendingProducers = copyProducerMap(s.PendingProducers)
@@ -173,9 +170,6 @@ func (s *StateKeyFrame) Serialize(w io.Writer) (err error) {
 		return
 	}
 
-	if err = s.SerializeStringMap(s.LastCRNodeOwnerKeys, w); err != nil {
-		return
-	}
 	if err = s.SerializeStringMap(s.CurrentCRNodeOwnerKeys, w); err != nil {
 		return
 	}
@@ -287,10 +281,6 @@ func (s *StateKeyFrame) Serialize(w io.Writer) (err error) {
 
 func (s *StateKeyFrame) Deserialize(r io.Reader) (err error) {
 	if s.NodeOwnerKeys, err = s.DeserializeStringMap(r); err != nil {
-		return
-	}
-
-	if s.LastCRNodeOwnerKeys, err = s.DeserializeStringMap(r); err != nil {
 		return
 	}
 
@@ -860,7 +850,6 @@ func NewStateKeyFrame() *StateKeyFrame {
 	info := make(map[string]common.Fixed64)
 	return &StateKeyFrame{
 		NodeOwnerKeys:             make(map[string]string),
-		LastCRNodeOwnerKeys:       make(map[string]string),
 		CurrentCRNodeOwnerKeys:    make(map[string]string),
 		NextCRNodeOwnerKeys:       make(map[string]string),
 		PendingProducers:          make(map[string]*Producer),

ce12a4601f9f7f3c3f76c88bb3371fa8009c7c09
jiangzehua <1092431698@qq.com>
2022-11-17 10:46:03 +0800

    feat(nft): add side chain format address to create nft tx payload

--- core/types/payload/createnft.go
+++ core/types/payload/createnft.go
@@ -17,7 +17,11 @@ const CreateNFTVersion byte = 0x00
 
 // CreateNFT defines the transaction of NFT.
 type CreateNFT struct {
+	// nft id, hash of detailed vote information.
 	ID common.Uint256
+
+	// side chain format address.
+	To string
 }
 
 func (a *CreateNFT) Data(version byte) []byte {
@@ -39,7 +43,11 @@ func (a *CreateNFT) Serialize(w io.Writer, version byte) error {
 
 func (a *CreateNFT) SerializeUnsigned(w io.Writer, version byte) error {
 	if err := a.ID.Serialize(w); err != nil {
-		return errors.New("[CRInfo], CID serialize failed")
+		return errors.New("[CreateNFT], failed to serialize ID")
+	}
+
+	if err := common.WriteVarString(w, a.To); err != nil {
+		return errors.New("[CreateNFT], failed to serialize To address")
 	}
 
 	return nil
@@ -56,8 +64,14 @@ func (a *CreateNFT) Deserialize(r io.Reader, version byte) error {
 
 func (a *CreateNFT) DeserializeUnsigned(r io.Reader, version byte) error {
 	if err := a.ID.Deserialize(r); err != nil {
-		return errors.New("[CRInfo], CID deserialize failed")
+		return errors.New("[CreateNFT], failed to deserialize ID")
 	}
+
+	to, err := common.ReadVarString(r)
+	if err != nil {
+		return errors.New("[CreateNFT], failed to deserialize To address")
+	}
+	a.To = to
 
 	return nil
 }

7c466ec7dc01aea74f2dee969fb3360400ebdfb2
xiaoming <1406629722@qq.com>
2022-11-24 14:04:51 +0800

    feat(dpos2.0): producer info support multi sign code

--- core/types/payload/producerinfo.go
+++ core/types/payload/producerinfo.go
@@ -18,6 +18,7 @@ const (
 	ProducerInfoVersion        byte = 0x00
 	ProducerInfoDposV2Version  byte = 0x01
 	ProducerInfoSchnorrVersion byte = 0x02
+	ProducerInfoMultiVersion   byte = 0x03
 )
 
 type ProducerInfo struct {
@@ -29,6 +30,7 @@ type ProducerInfo struct {
 	NetAddress     string
 	StakeUntil     uint32
 	Signature      []byte
+	MultiCode      []byte
 }
 
 func (a *ProducerInfo) Data(version byte) []byte {
@@ -51,18 +53,28 @@ func (a *ProducerInfo) Serialize(w io.Writer, version byte) error {
 		}
 	}
 
+	if version == ProducerInfoMultiVersion {
+		err = common.WriteVarBytes(w, a.MultiCode)
+		if err != nil {
+			return errors.New("[ProducerInfo], MultiCode serialize failed")
+		}
+	}
+
 	return nil
 }
 
 func (a *ProducerInfo) SerializeUnsigned(w io.Writer, version byte) error {
-	err := common.WriteVarBytes(w, a.OwnerPublicKey)
-	if err != nil {
-		return errors.New("[ProducerInfo], owner publicKey serialize failed")
-	}
+	var err error
+	if version != ProducerInfoMultiVersion {
+		err = common.WriteVarBytes(w, a.OwnerPublicKey)
+		if err != nil {
+			return errors.New("[ProducerInfo], owner publicKey serialize failed")
+		}
 
-	err = common.WriteVarBytes(w, a.NodePublicKey)
-	if err != nil {
-		return errors.New("[ProducerInfo], node publicKey serialize failed")
+		err = common.WriteVarBytes(w, a.NodePublicKey)
+		if err != nil {
+			return errors.New("[ProducerInfo], node publicKey serialize failed")
+		}
 	}
 
 	err = common.WriteVarString(w, a.NickName)
@@ -105,21 +117,28 @@ func (a *ProducerInfo) Deserialize(r io.Reader, version byte) error {
 			return errors.New("[ProducerInfo], signature deserialize failed")
 		}
 	}
+	if version == ProducerInfoMultiVersion {
+		a.MultiCode, err = common.ReadVarBytes(r, crypto.MaxMultiSignCodeLength, "multicode")
+		if err != nil {
+			return errors.New("[ProducerInfo], signature deserialize failed")
+		}
+	}
 	return nil
 }
 
 func (a *ProducerInfo) DeserializeUnsigned(r io.Reader, version byte) error {
 	var err error
-	a.OwnerPublicKey, err = common.ReadVarBytes(r, crypto.NegativeBigLength, "own public key")
-	if err != nil {
-		return errors.New("[ProducerInfo], owner publicKey deserialize failed")
-	}
+	if version != ProducerInfoMultiVersion {
+		a.OwnerPublicKey, err = common.ReadVarBytes(r, crypto.NegativeBigLength, "own public key")
+		if err != nil {
+			return errors.New("[ProducerInfo], owner publicKey deserialize failed")
+		}
 
-	a.NodePublicKey, err = common.ReadVarBytes(r, crypto.NegativeBigLength, "node public key")
-	if err != nil {
-		return errors.New("[ProducerInfo], node publicKey deserialize failed")
+		a.NodePublicKey, err = common.ReadVarBytes(r, crypto.NegativeBigLength, "node public key")
+		if err != nil {
+			return errors.New("[ProducerInfo], node publicKey deserialize failed")
+		}
 	}
-
 	a.NickName, err = common.ReadVarString(r)
 	if err != nil {
 		return errors.New("[ProducerInfo], nickName deserialize failed")

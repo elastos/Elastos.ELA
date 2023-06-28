1ba798b49e61c8c21d2d353c99a433459e564bf2
jiangzehua <1092431698@qq.com>
2023-06-28 17:20:10 +0800

    feat(bpos): create IllegalProposal and IllegalVote evidence after ChangeViewV1Height

--- dpos/manager/proposaldispatcher.go
+++ dpos/manager/proposaldispatcher.go
@@ -110,10 +110,12 @@ func (p *ProposalDispatcher) ProcessVote(v *payload.DPOSProposalVote,
 		return false, false
 	}
 
-	//if anotherVote, legal := p.illegalMonitor.IsLegalVote(v); !legal {
-	//	p.illegalMonitor.ProcessIllegalVote(v, anotherVote)
-	//	return
-	//}
+	if p.finishedHeight > p.cfg.ChainParams.DPoSConfiguration.ChangeViewV1Height {
+		if anotherVote, legal := p.illegalMonitor.IsLegalVote(v); !legal {
+			p.illegalMonitor.ProcessIllegalVote(v, anotherVote)
+			return
+		}
+	}
 
 	if accept {
 		return p.countAcceptedVote(v)
@@ -309,10 +311,12 @@ func (p *ProposalDispatcher) ProcessProposal(id peer.PID, d *payload.DPOSProposa
 		}
 	}
 
-	//if anotherProposal, ok := p.illegalMonitor.IsLegalProposal(d); !ok {
-	//	p.illegalMonitor.ProcessIllegalProposal(d, anotherProposal)
-	//	return true, true
-	//}
+	if p.finishedHeight > p.cfg.ChainParams.DPoSConfiguration.ChangeViewV1Height {
+		if anotherProposal, ok := p.illegalMonitor.IsLegalProposal(d); !ok {
+			p.illegalMonitor.ProcessIllegalProposal(d, anotherProposal)
+			return true, true
+		}
+	}
 
 	if !p.cfg.Consensus.IsArbitratorOnDuty(d.Sponsor) {
 		currentArbiter := p.cfg.Manager.GetArbitrators().GetNextOnDutyArbitrator(p.cfg.Consensus.GetViewOffset())

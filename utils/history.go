9016786b05a07a4f5469b8639ae6d0bd9b52b84a
jzh <1092431698@qq.com>
2025-01-17 11:53:13 +0800

    fix(dpos): remove history by real changes

--- utils/history.go
+++ utils/history.go
@@ -132,9 +132,23 @@ func (h *History) Commit(height uint32) {
 		h.changes[i].commit()
 	}
 
+	// same height changes is just one
+	holdHeight := make(map[uint32]bool, 0)
+	trueChange := 0
+	lastChangeCount := 0
+	for i, change := range h.changes {
+		if i == 0 {
+			lastChangeCount++
+		}
+		_, ok := holdHeight[change.height]
+		if !ok {
+			trueChange++
+			holdHeight[change.height] = true
+		}
+	}
 	// if changes overflows history's capacity, remove the oldest change.
-	if len(h.changes) >= h.capacity {
-		h.changes = h.changes[1:]
+	if trueChange >= h.capacity {
+		h.changes = h.changes[lastChangeCount:]
 	}
 
 	// if no cached changes, create an empty height change.
